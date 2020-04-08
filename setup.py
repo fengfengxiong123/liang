2f04049e2cfc7e76e44daf20b9b284933df36516
liang <359975715@qq.com>
2020-04-08 18:15:39 +0800

    1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='clinicaltrial',
+      version='0.1',
+      description='clinical trial information retriver',
+      url='http://github.com/tongling/clinicaltrial',
+      author='Ling',
+      author_email='359975715@qq.com',
+      license='MIT',
+      packages=['clinicaltrial','filter'],
+      zip_safe=False)
