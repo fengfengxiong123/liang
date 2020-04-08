be95f23ebcedebd0fa3e0a135bf6e977380bcf12
liang <359975715@qq.com>
2020-04-08 18:51:02 +0800

    创建自己的PyPi

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 setup(name='liang',
-      version='0.0.1',
+      version='0.0.2',
       description='only me',
       url='https://github.com/fengfengxiong123/liang.git',
-      author='liang',
+      author='梁峰',
       author_email='359975715@qq.com',
       license='MIT',
       packages=['liang'],
